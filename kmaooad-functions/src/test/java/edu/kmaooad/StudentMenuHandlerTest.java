ddcf5b890f5a10dc670423963869db5d921b7db0
Mishok <mishaonukrnet@ukr.net>
2022-11-20 14:54:38 +0200

    Add tests for StudentMenuHandler

---
+++ kmaooad-functions/src/test/java/edu/kmaooad/StudentMenuHandlerTest.java
@@ -0,0 +1,62 @@
+package edu.kmaooad;
+
+import edu.kmaooad.constants.bot.ConversationState;
+import edu.kmaooad.handler.impl.CancelHandler;
+import edu.kmaooad.handler.impl.StudentMenuHandler;
+import edu.kmaooad.helper.KeyboardHelper;
+import edu.kmaooad.model.UserRequest;
+import edu.kmaooad.model.UserSession;
+import edu.kmaooad.service.TelegramService;
+import edu.kmaooad.service.UserSessionService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+public class StudentMenuHandlerTest {
+    private StudentMenuHandler studentMenuHandler;
+
+    @Mock
+    private TelegramService telegramService;
+    private KeyboardHelper keyboardHelper = new KeyboardHelper();
+    private UserSessionService userSessionService = new UserSessionService();
+
+    @BeforeEach
+    public void initTests() {
+        MockitoAnnotations.openMocks(this);
+        Mockito.doReturn(null).when(telegramService).sendMessage(Mockito.any(Long.class), Mockito.any(String.class), Mockito.any(ReplyKeyboard.class));
+        studentMenuHandler = new StudentMenuHandler(telegramService, keyboardHelper, userSessionService);
+    }
+
+    @Test
+    public void testHandleMethod() {
+        UserSession session = UserSession.builder().state(ConversationState.WAITING_FOR_TEXT).build();
+        studentMenuHandler.handle(UserRequest.builder().userSession(session).build());
+
+        Assertions.assertEquals(session.getState(), ConversationState.WAITING_FOR_CHOICE);
+    }
+
+    @Test
+    public void testIsApplicable(){
+        Message messageApplicable = new Message();
+        messageApplicable.setText(StudentMenuHandler.STUDENTS_MAIN);
+
+        Update updateApplicable = new Update();
+        updateApplicable.setMessage(messageApplicable);
+
+        UserRequest userRequestApplicable = UserRequest.builder().chatId(1L).update(updateApplicable).build();
+
+        Assertions.assertTrue(studentMenuHandler.isApplicable(userRequestApplicable));
+    }
+}
