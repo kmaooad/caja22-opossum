d495cf21b3bacefd02c1ac06bc1192f9dde293fe
Mishok <mishaonukrnet@ukr.net>
2022-11-21 02:37:37 +0200

    Added structure for group operations

--- kmaooad-functions/src/main/java/edu/kmaooad/handler/impl/group/GroupButtonsHandler.java
+++ kmaooad-functions/src/main/java/edu/kmaooad/handler/impl/group/GroupButtonsHandler.java
@@ -1,4 +1,4 @@
-package edu.kmaooad.handler.impl.students;
+package edu.kmaooad.handler.impl.group;
 
 import edu.kmaooad.constants.bot.ConversationState;
 import edu.kmaooad.handler.UserRequestHandler;
@@ -7,23 +7,21 @@ import edu.kmaooad.model.UserRequest;
 import edu.kmaooad.model.UserSession;
 import edu.kmaooad.service.TelegramService;
 import edu.kmaooad.service.UserSessionService;
-import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 
 import java.util.List;
 
-@Component
-public class StudentButtonsHandler extends UserRequestHandler {
+public class GroupButtonsHandler extends UserRequestHandler {
 
-    public static String STUDENTS_MAIN = "Студенти";
-    public static List<String> cities = List.of("Додати студентів з csv", "Оновити студентів з csv");
+    public static String STUDENTS_MAIN = "Групи";
+    public static List<String> cities = List.of("Редагувати", "Видалити", "Змінити");
 
     private final TelegramService telegramService;
     private final KeyboardHelper keyboardHelper;
     private final UserSessionService userSessionService;
 
-    public StudentButtonsHandler(TelegramService telegramService, KeyboardHelper keyboardHelper, UserSessionService userSessionService) {
+    public GroupButtonsHandler(TelegramService telegramService, KeyboardHelper keyboardHelper, UserSessionService userSessionService) {
         this.telegramService = telegramService;
         this.keyboardHelper = keyboardHelper;
         this.userSessionService = userSessionService;
@@ -41,7 +39,7 @@ public class StudentButtonsHandler extends UserRequestHandler {
         userSession.setState(ConversationState.WAITING_FOR_CHOICE);
         userSessionService.saveSession(userSession.getChatId(), userSession);
 
-        return telegramService.sendMessage(userRequest.getChatId(),"Оберіть що хочете робити⤵️", replyKeyboardMarkup);
+        return telegramService.sendMessage(userRequest.getChatId(), "Оберіть що хочете робити⤵️", replyKeyboardMarkup);
     }
 
     @Override
---
+++ kmaooad-functions/src/main/java/edu/kmaooad/handler/impl/group/update/UpdateGroupHandler.java
@@ -0,0 +1,29 @@
+package edu.kmaooad.handler.impl.group.update;
+
+import edu.kmaooad.handler.UserRequestHandler;
+import edu.kmaooad.model.UserRequest;
+import edu.kmaooad.service.GroupService;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+
+public class UpdateGroupHandler extends UserRequestHandler {
+    private final GroupService groupService;
+
+    public UpdateGroupHandler(GroupService groupService) {
+        this.groupService = groupService;
+    }
+
+    @Override
+    public boolean isApplicable(UserRequest request) {
+        return false;
+    }
+
+    @Override
+    public BotApiMethod<?> handle(UserRequest dispatchRequest) {
+        return null;
+    }
+
+    @Override
+    public boolean isGlobal() {
+        return false;
+    }
+}
