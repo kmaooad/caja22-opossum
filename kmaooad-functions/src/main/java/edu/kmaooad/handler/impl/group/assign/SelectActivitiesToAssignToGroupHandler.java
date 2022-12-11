9fcd7dc89947eea22a9595417616ae3b53a22f09
BlackKyiv <62792985+BlackKyiv@users.noreply.github.com>
2022-12-11 19:17:21 +0100

    Fixed adding and removing activities to group

--- kmaooad-functions/src/main/java/edu/kmaooad/handler/impl/group/assign/SelectActivitiesToAssignToGroupHandler.java
+++ kmaooad-functions/src/main/java/edu/kmaooad/handler/impl/group/assign/SelectActivitiesToAssignToGroupHandler.java
@@ -1,6 +1,7 @@
 package edu.kmaooad.handler.impl.group.assign;
 
 import edu.kmaooad.constants.bot.ConversationState;
+import edu.kmaooad.constants.bot.GlobalConstants;
 import edu.kmaooad.constants.bot.GroupConstants;
 import edu.kmaooad.handler.ButtonRequestHandler;
 import edu.kmaooad.handler.UserRequestHandler;
@@ -42,9 +43,12 @@ public class SelectActivitiesToAssignToGroupHandler implements ButtonRequestHand
     @Override
     public HandlerResponse handle(UserRequest userRequest) {
         log.info(userRequest.toString());
-        Group group = groupService.getGroupByName(userRequest.getUpdate().getMessage().getText());
+        String text = userRequest.getUpdate().getMessage().getText();
+        Group group = groupService.getGroupByName(text.trim());
+        log.info("User picked group by name " + text + ", found group " + group);
+        userRequest.getUserSession().getData().put(GroupConstants.GROUP_MAP_KEY, group);
         ReplyKeyboardMarkup replyKeyboardMarkup = keyboardHelper.buildAdditionalActionsVertical(activityService.getStatusOfActivitiesForGroup(group));
-        userRequest.getUserSession().setConversationState(ConversationState.WAITING_FOR_GROUP_TO_ASSIGN_CHOICE);
+        userRequest.getUserSession().setConversationState(ConversationState.WAITING_FOR_GROUP_ACTIVITY_ASSIGN_CHOICE);
 
         return new HandlerResponse(telegramService.sendMessage(userRequest.getChatId(), "Оберіть активність яку хочете додати до " + group.getName() + " ⤵️", replyKeyboardMarkup), true);
     }
--- kmaooad-functions/src/main/java/edu/kmaooad/service/ActivityService.java
+++ kmaooad-functions/src/main/java/edu/kmaooad/service/ActivityService.java
@@ -34,16 +34,16 @@ public class ActivityService {
         List<Activity> allActivities = activityRepository.findAll();
         return allActivities.stream()
                 .map(activity -> {
-                    if (group.getActivities().contains(activity)) {
-                        return activity.getName() + ASSIGNED;
+                    if (group.getActivities().contains(activity.getId())) {
+                        return activity.getName() + " "  + ASSIGNED;
                     } else return activity.getName();
                 })
                 .collect(Collectors.toList());
     }
 
-    public Activity getActivityByName(String name){
+    public Activity getActivityByName(String name) {
         Optional<Activity> group = activityRepository.findByName(name);
-        if(group.isPresent()){
+        if (group.isPresent()) {
             return group.get();
         } else {
             log.warn("Group not found, name: " + name);
@@ -52,18 +52,18 @@ public class ActivityService {
     }
 
     public List<Activity> updateActivities(List<Activity> activityList) throws ServiceException {
-        for (Activity a:activityList) {
-        Optional<Activity> activity = activityRepository.findById(a.getId());
-        if (activity.isPresent()) {
-            Activity activityFound = activity.get();
-            activityFound.setName( a.getName());
-            activityFound.setStatus( a.getStatus());
-            activityFound.setStartDate( a.getStartDate());
-            activityFound.setEndDate( a.getEndDate());
-            activityRepository.save(activityFound);
-        }else{
-            throw new ServiceException("Failed to update activity: "+activity + " activity with such id does not exists in database");
-        }
+        for (Activity a : activityList) {
+            Optional<Activity> activity = activityRepository.findById(a.getId());
+            if (activity.isPresent()) {
+                Activity activityFound = activity.get();
+                activityFound.setName(a.getName());
+                activityFound.setStatus(a.getStatus());
+                activityFound.setStartDate(a.getStartDate());
+                activityFound.setEndDate(a.getEndDate());
+                activityRepository.save(activityFound);
+            } else {
+                throw new ServiceException("Failed to update activity: " + activity + " activity with such id does not exists in database");
+            }
         }
         return activityList;
     }
@@ -73,7 +73,7 @@ public class ActivityService {
         Optional<Activity> found = activityRepository.findByName(activity.getName());
 
         if (found.isPresent()) {
-            throw new ServiceException("Failed to add activity: "+activity + " activity with such values exists in database");
+            throw new ServiceException("Failed to add activity: " + activity + " activity with such values exists in database");
 
         } else {
             activityRepository.save(activity);
