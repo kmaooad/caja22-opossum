7b02e366d79f4126d6185c568a4c05d2d9154a20
KateKolokhina <39592816+KateKolokhina@users.noreply.github.com>
2022-12-12 05:40:20 +0200

    Group-template fixed

--- kmaooad-functions/src/main/java/edu/kmaooad/service/GroupTemplateService.java
+++ kmaooad-functions/src/main/java/edu/kmaooad/service/GroupTemplateService.java
@@ -1,38 +1,63 @@
 package edu.kmaooad.service;
 
+import edu.kmaooad.model.Group;
 import edu.kmaooad.model.GroupTemplate;
-import edu.kmaooad.model.Student;
 import edu.kmaooad.repositories.GroupTemplateRepository;
-import edu.kmaooad.repositories.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GroupTemplateService {
     @Autowired
-    private GroupTemplateRepository repository;
+    private GroupTemplateRepository groupTemplateRepository;
 
+    /**
+     * @param groupTemplate  - group template to add
+     * @return added template updated, if already exists - throws exception
+     */
+    public GroupTemplate addGroupTemplate(GroupTemplate groupTemplate) throws ServiceException {
 
-    public boolean addGroupTemplate(GroupTemplate groupTemplate) {
-        // яка перевірка на унікальність в бд?
-
-        return false;
+        if (null == groupTemplateRepository.findByNameAndYearAndGrade(groupTemplate.getName(), groupTemplate.getYear(), groupTemplate.getGrade())){
+            groupTemplateRepository.save(groupTemplate);
+            return groupTemplate;
+        }else{
+            throw new ServiceException("Failed to add group template: "+groupTemplate + " group with such values exists in database");
+        }
     }
 
-    public boolean updateGroupTemplate(GroupTemplate groupTemplate) {
-        // яка перевірка на унікальність в бд?
-
-        return false;
+    /**
+     * @param groupTemplateUpdate  - group template to updated
+     * @return group template updated, if not found - throws exception
+     */
+    public GroupTemplate updateGroupTemplate(GroupTemplate groupTemplateUpdate) throws ServiceException {
+        Optional<GroupTemplate> group = groupTemplateRepository.findById(groupTemplateUpdate.getId());
+        if (group.isPresent()) {
+            GroupTemplate groupFound = group.get();
+            groupFound.setName(groupTemplateUpdate.getName());
+            groupFound.setGrade(groupTemplateUpdate.getGrade());
+            groupFound.setYear(groupTemplateUpdate.getYear());
+             groupTemplateRepository.save(groupFound);
+            return groupFound;
+        }else{
+            throw new ServiceException("Failed to update group template: "+groupTemplateUpdate + " group with such id doesn`t exists in database");
+        }
     }
 
-
-    public boolean deleteGroupTemplate(String groupTemplateId) {
-
-        //   return repository.delete(groupTemplateId);
-
-        return false;
+    /**
+     * @param groupTemplateId  - group template to delete
+     * @return group template deleted, if not found - throws exception
+     */
+    public GroupTemplate deleteGroupTemplate(String groupTemplateId) throws ServiceException {
+        Optional<GroupTemplate> group = groupTemplateRepository.findById(groupTemplateId);
+        if (group.isPresent()) {
+            GroupTemplate groupFound = group.get();
+            groupTemplateRepository.delete(groupFound);
+            return groupFound;
+        }else{
+            throw new ServiceException("Failed to delete group template with id: "+groupTemplateId + " group with such id doesn`t exists in database");
+        }
     }
 
 
