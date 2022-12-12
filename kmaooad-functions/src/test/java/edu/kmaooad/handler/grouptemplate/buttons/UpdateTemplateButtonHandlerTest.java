e5a8a199aa3ad972334de61c4fa9c826263355d5
Iryna Zubrytska <zubrytska.ira@gmail.com>
2022-12-12 12:51:27 +0100

    added tests for templates button handlers

---
+++ kmaooad-functions/src/test/java/edu/kmaooad/handler/grouptemplate/buttons/UpdateTemplateButtonHandlerTest.java
@@ -0,0 +1,81 @@
+package edu.kmaooad.handler.grouptemplate.buttons;
+
+import edu.kmaooad.constants.bot.ConversationState;
+import edu.kmaooad.constants.bot.GroupConstants;
+import edu.kmaooad.constants.bot.GroupTemplateConstants;
+import edu.kmaooad.handler.impl.template.button.UpdateTemplateButtonHandler;
+import edu.kmaooad.handler.impl.template.dialog.UpdateTemplateDialog;
+import edu.kmaooad.model.HandlerResponse;
+import edu.kmaooad.model.UserRequest;
+import edu.kmaooad.model.UserSession;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+public class UpdateTemplateButtonHandlerTest {
+
+    @InjectMocks
+    private UpdateTemplateButtonHandler updateTemplateButtonHandler;
+
+    @Mock
+    private UpdateTemplateDialog updateTemplateDialog;
+
+    private Message message;
+    private Update update;
+    private UserSession userSession;
+    private UserRequest userRequest;
+
+    @BeforeEach
+    public void initTests(){
+        message = new Message();
+        update = new Update();
+        update.setMessage(message);
+        userSession = UserSession.builder()
+                .chatId(1L)
+                .build();
+        userRequest = UserRequest.builder().update(update).userSession(userSession).build();
+
+        Mockito.when(updateTemplateDialog.startDialog(Mockito.any(UserRequest.class)))
+                .thenReturn(new HandlerResponse(null, true));
+    }
+
+    @Test
+    public void testIsNotApplicableConversationState(){
+        message.setText(GroupTemplateConstants.GROUP_TEMPLATE_EDIT_BUTTON_LABEL);
+        userSession.setConversationState(ConversationState.CONVERSATION_STARTED);
+        Assertions.assertFalse(updateTemplateButtonHandler.isApplicable(userRequest));
+    }
+
+    @Test
+    public void testIsNotApplicableMessage(){
+        message.setText("Not applicable");
+        userSession.setConversationState(ConversationState.WAITING_FOR_GROUP_ACTION_CHOICE);
+        Assertions.assertFalse(updateTemplateButtonHandler.isApplicable(userRequest));
+    }
+
+    @Test
+    public void testIsApplicable(){
+        message.setText(GroupTemplateConstants.GROUP_TEMPLATE_EDIT_BUTTON_LABEL);
+        userSession.setConversationState(ConversationState.WAITING_FOR_TEMPLATE_ACTION_CHOICE);
+        Assertions.assertTrue(updateTemplateButtonHandler.isApplicable(userRequest));
+    }
+
+    @Test
+    public void testHandler(){
+        HandlerResponse handlerResponse = updateTemplateButtonHandler.handle(userRequest);
+
+        Assertions.assertNull(handlerResponse.getResult());
+        Assertions.assertTrue(handlerResponse.isSuccess());
+    }
+}
